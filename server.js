b527e8a572c9da1135f59e6fb56b839e006da8d5
Edwin Ovie Luther <binary01ltd@outlook.com>
2017-10-28 23:14:08 +0100

    Ethercrm

--- server.js
+++ server.js
@@ -2,22 +2,14 @@ var express = require('express'),
   passport = require('passport'),
   BasicStrategy = require('passport-http').BasicStrategy;
 
-
-// Use the BasicStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.
 var app = express();
 
 app.use(passport.initialize());
-// comment/remove this line below to disable auth
-app.use(passport.authenticate('basic', {
-  session: false
-}));
+
 app.use(express.static(__dirname + '/public'));
 
 
-var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 3000);
 app.listen(port, function () {
   console.log("Listening on " + port);
 });
