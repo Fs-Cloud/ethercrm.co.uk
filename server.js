bb7ca715368df53077e4d5729883199414409e7f
Edwin Ovie Luther <binary01ltd@outlook.com>
2017-10-30 22:20:28 +0000

    ed

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-var express = require('express'),
-  passport = require('passport'),
-  BasicStrategy = require('passport-http').BasicStrategy;
+var express = require('express');
+
 
 var app = express();
 
-app.use(passport.initialize());
-// comment/remove this line below to disable auth
-app.use(passport.authenticate('basic', {
-  session: false
-}));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));;
 
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function () {
-      console.log("Listening on " + port);
+var port = Number(process.env.PORT || 3000);
+app.listen(port, function() {
+console.log("Listening on " + port);
 });
