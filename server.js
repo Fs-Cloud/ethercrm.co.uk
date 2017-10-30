5a5a3d44dfcb40fd2d3930fb51278a22999bdc9e
eetadafe86 <edwin-luther@hotmail.com>
2017-10-30 12:31:05 +0000

    ed
    ed

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-var express = require('express'),
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 4567,
+    publicDir = process.argv[2] || __dirname + '/public',
+    path = require('path');
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function () {
-      console.log("Listening on " + port);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(publicDir, "/index.html"));
 });
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+app.listen(port, hostname);
