54109e4f2a96970e089e3465edf618442ad9e1bd
eetadafe86 <edwin-luther@hotmail.com>
2017-10-30 12:21:21 +0000

    ED
    ED

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 var express = require('express'),
-  passport = require('passport'),
-  BasicStrategy = require('passport-http').BasicStrategy;
 
 var app = express();
-
-app.use(passport.initialize());
-// comment/remove this line below to disable auth
-app.use(passport.authenticate('basic', {
-  session: false
-}));
 app.use(express.static(__dirname + '/public'));
-
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function () {
