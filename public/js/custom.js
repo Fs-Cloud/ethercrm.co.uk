c306a3e6d0d49196b86cd2c58e91fcd72743c4dc
Ed <edwin-luther@hotmail.com>
2017-11-19 08:49:22 +0000

    ed

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,27 +1,27 @@
-(function ($) {
+(function($) {
     'use strict';
 
-    $(document).on('ready', function () {
+    $(document).on('ready', function() {
         // -----------------------------
         //  Testimonial Slider
         // -----------------------------
         $('.testimonial-slider').owlCarousel({
-            loop:true,
-            margin:20,
-            dots:true,
-            autoplay:true,
-            responsive:{
-                0:{
-                    items:1
+            loop: true,
+            margin: 20,
+            dots: true,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1
                 },
-                400:{
-                    items:1
+                400: {
+                    items: 1
                 },
-                600:{
-                    items:1
+                600: {
+                    items: 1
                 },
-                1000:{
-                    items:2
+                1000: {
+                    items: 2
                 }
             }
         });
@@ -29,52 +29,52 @@
         //  Story Slider
         // -----------------------------
         $('.about-slider').owlCarousel({
-            loop:true,
-            margin:20,
-            dots:true,
-            autoplay:true,
-            items : 1
+            loop: true,
+            margin: 20,
+            dots: true,
+            autoplay: true,
+            items: 1
         });
         // -----------------------------
         //  Quote Slider
         // -----------------------------
         $('.quote-slider').owlCarousel({
-            loop:true,
-            autoplay:true,
-            items : 1
+            loop: true,
+            autoplay: true,
+            items: 1
         });
         // -----------------------------
         //  On Click Smooth scrool
         // -----------------------------
-         $('.scrollTo').on('click', function(e) {
-             e.preventDefault();
-             var target = $(this).attr('href');
-             $('html, body').animate({
-               scrollTop: ($(target).offset().top)
-             }, 500);
-          });
+        $('.scrollTo').on('click', function(e) {
+            e.preventDefault();
+            var target = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: ($(target).offset().top)
+            }, 500);
+        });
         // -----------------------------
         //  Client Slider
         // -----------------------------
         $('.client-slider').owlCarousel({
-            loop:true,
-            autoplay:true,
+            loop: true,
+            autoplay: true,
             margin: 50,
-            responsive:{
-                0:{
-                    items:1,
-                    dots:false
+            responsive: {
+                0: {
+                    items: 1,
+                    dots: false
                 },
-                400:{
-                    items:2,
-                    dots:false
+                400: {
+                    items: 2,
+                    dots: false
                 },
-                600:{
-                    items:2,
-                    dots:false
+                600: {
+                    items: 2,
+                    dots: false
                 },
-                1000:{
-                    items:4
+                1000: {
+                    items: 4
                 }
             }
         });
@@ -102,7 +102,7 @@
         function initialize() {
             var styleArray = [];
 
-            var myLatLng = {lat: 52.188933, lng: 33.083869};
+            var myLatLng = { lat: 52.188933, lng: 33.083869 };
 
             var mapOptions = {
                 zoom: 7,
@@ -112,7 +112,7 @@
             };
 
             var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-            
+
             var marker = new google.maps.Marker({
                 position: myLatLng,
                 map: map,
